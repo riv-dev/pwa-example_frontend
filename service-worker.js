b36b27f106a240fdaed320b404eaab8a1c6fb933
Ken Le <kenle545@gmail.com>
2017-12-04 16:19:02 +0700

    Fix fetching issue and update icon sizes

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,7 @@ var filesToCache = [
   '/',
   '/index.html',
   '/javascripts/common.js',
+  '/javascripts/jquery-3.2.1.min.js',
   '/stylesheets/common.css',
   '/images/logo.png',
   '/images/button-a.png',
@@ -12,6 +13,7 @@ var filesToCache = [
   '/images/button-c.png'
 ];
 
+//Use install to cache shell, images, css, and javascript
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
@@ -22,6 +24,7 @@ self.addEventListener('install', function(e) {
   );
 });
 
+//Use activate event listener to remove old caches when updating cache key version
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
@@ -37,6 +40,7 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
+//Use fetch to intercept HTTP requests in order to cache
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
 
